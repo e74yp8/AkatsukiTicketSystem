aa0457e55eb9638bb81e5ded4355bf00bc4ceb07
e74yp8 <williamhon404@gmail.com>
2024-06-24 23:50:10 +0800

    cleaning

--- TicketBackend/main.go
+++ TicketBackend/main.go
@@ -4,26 +4,18 @@ import (
 	"TicketBackend/api"
 	"TicketBackend/sql"
 	"TicketBackend/ticket"
-	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	sql.ConnectSQL()
-	//ticket.TableInit()
-
-	ticket.Create("1234567890")
-	status, err := ticket.CheckStatus("1234567890")
-	if err != nil {
-		fmt.Printf("Check Status Failed, Error: %v\n", err.Error())
-	}
-	fmt.Printf("Ticket Status: %v\n", status)
+	ticket.TableInit()
 
 	//gin
 	router := gin.Default()
 	router.GET("/api/ticket/:ticketCode", api.TicketAPI)
 	err2 := router.Run(":8080")
-	if err != nil {
+	if err2 != nil {
 		panic("Start Router Failed, Error:" + err2.Error())
 	}
 }
